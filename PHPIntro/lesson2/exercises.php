d73a7468898fe87863f143d015c9a8f4823030c9
Mindaugas <mindaugas.g@rrr.lt>
2022-10-23 23:11:52 +0300

    Create lesson2 exercises

---
+++ PHPIntro/lesson2/exercises.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+1. Apskaičiuokite PHP pagalba ir išveskite į terminalą. Kiekvienas rezultatas turi būti naujoje eilutėje:
+- 987 + 545 - 32 * 94
+- 32 pakelkite laipsniu 3 ir pridėkite 18
+- 120 padalinkite iš 4 ir dar karta padalinkite iš 3
+- apkaičiuokite liekaną po skaičiaus 187 dalybos iš 5
+- skaičiui 3 tris kartus pritaikykite increment operatorių - koki skaičių gaunate?
+- skaičiui 12 keturis kartus pritaikykite decrement operatorių - koki skaičių gaunate?
+*/
+
+/*
+2. Išspausdinkite tekstą, kuris bus sudarytas sujungus visas eilutes, esančias kintamuosiuose apačioje.
+$part1 = 'This';
+$part2 = 'is';
+$part3 = 'called';
+$part4 = 'string';
+$part5 = 'concatenation';
+*/
+
+/*
+3. Jūs turite parašyti sąlygą, pagal kurią bus taikoma nuolaida produktų kategorijoms.
+Parašykite sąlygą, kuri būtų true, kai:
+- produkto kategorija yra 'smartphone' ir kaina yra daugiau arba lygu 1200eur
+ARBA
+- produkto kategorija yra 'washing_machine' ir kaina yra tarp 500eur ir 700eur (įskaitant 500).
+Sąlygos rezultatą priskirkite kintamajam $result ir šį kintamąjį išveskite į terminalą pasinaudodami var_dump.
+define('CATEGORY_SMARTPHONE', 'smartphone');
+define('CATEGORY_WASHING_MACHINE', 'washing_machine');
+$productCategory = CATEGORY_SMARTPHONE;
+$productPrice = 1200;
+*/
