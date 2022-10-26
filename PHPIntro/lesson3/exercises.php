4042c9765e485d7dc48699aa9ca01546281e34aa
Mindaugas <mindaugas.g@rrr.lt>
2022-10-26 10:20:47 +0300

    Create lesson3-4 exercises

---
+++ PHPIntro/lesson3/exercises.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+1. Išspausdinkite skaičius nuo 1 iki 10 naudodamiesi ciklu. Panaudokite visus 4 būdus ciklams aprašyti.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+1
+2
+3
+...
+*/
+
+/*
+2. Išspausdinkite skaičius nuo 15 iki 3 naudodamiesi ciklu. Panaudokite sau patogiausią ciklą.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+3. Išspausdinkite kas antrą skaičių nuo 1 iki 20 naudodamiesi ciklu.
+Kiekvienas skaičius turi būti išspausdintas naujoje eilutėje.
+1
+3
+5
+...
+*/
+
+/*
+4. Išspausdinkite skaičius, nuo 1 iki 20, kurie dalijasi iš 3.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+5. Išspausdinkite skaičius, nuo 1 iki 20, kurie dalijasi iš 3 arba iš 5.
+Kiekvienas skaičius turi išspausdintas naujoje eilutėje.
+*/
+
+/*
+6. Raskite sveikų skaičių nuo 1 iki 100 sumą.
+*/
+
+/*
+7. Pasinaudodami ciklu išspausdinkite savaitės dienas iš masyvo $days vienoje eilutėje:
+monday-tuesday-wednesday-thursday-friday-saturday-sunday-
+*/
+
+$days = [
+    'monday',
+    'tuesday',
+    'wednesday',
+    'thursday',
+    'friday',
+    'saturday',
+    'sunday',
+];
+
+/*
+8. Skaičių konvertavimas į bool.
+Iteruokite sveikus skaičius nuo -5 iki 5.
+Išspausdinkite kiekvieną skaičių dvejopai:
+1. Pasinaudojant paprastu echo
+2. Pasinaudojant funkcija var_dump ir prieš tai pavertus į 'bool' tipo reikšmę
+-5
+bool(true)
+----------
+-4
+bool(true)
+----------
+...
+Atkreipkite dėmesį, į ką pavirsta skaičius 0
+*/
+
+/*
+9. Foreach'inkite per masyvą ir išspausdinkite jame esančias eilutes.
+Jeigu iteracijos tekstas yra vienas iš: '/', '*' arba '()', jo nespausdinkite, teskite iteraciją.
+Jeigu interacijos tekstas yra lygus '---', teksto nespausdinti, o ciklas turėtų nutrūkti.
+Užduočiai atlikti naudokite 'continue' ir 'break'.
+$items = [
+    'Lazy',
+    '/',
+    'fox',
+    'jumps',
+    '*',
+    'over',
+    'the',
+    '()',
+    'lazy',
+    'dog',
+    '---',
+    'this',
+    'should',
+    'not',
+    'be',
+    'printed',
+    'or',
+    'iterated',
+];
+*/
+
+/*
+10. Iteruokite per skaičius, nuo 1 iki 20.
+Jeigu skaičius dalijasi iš 3, išspausdinkite žodį 'Hey'.
+Jeigu skaičius dalijasi iš 5, išspausdinkite žodį Ho'.
+Jeigu skaičius dalijasi ir iš 5, ir iš 3, išspausdinkite žodį 'HeyHo'.
+Kitu atveju išspausdinkite skaičių.
+Viskas turi būti atspausdinta vienoje eilutėje su tarpais:
+1 2 Hey 4 Ho Hey 7 8 Hey Ho 11 Hey 13 14 HeyHo 16 17 Hey 19 Ho
+*/
+
+/*
+11. Pasinaudodami ciklais, išspausdinkite lentelę, kurioje nelyginiai skaičiai yra žvaigždutės.
+Parašykite kodą taip, kad jis su menkais pakeitimais veiktų ir tuo atveju, jeigu reiktų spausdinti
+ne iki 20, bet iki kito dviženklio skaičiaus (pvz. 90).
+* |2 |* |4 |*
+6 |* |8 |* |10
+* |12|* |14|*
+16|* |18|* |20
+*/
+
+/*
+12. Pasinaudodami ciklais, išspausdinkite į terminalą šią figūrą.
+*
+**
+***
+****
+*****
+******
+*******
+******
+*****
+****
+***
+**
+*
+*/
+
+/*
+13. Išspausdinkite stačiakampį, kurio matmenys bus kinamųjų reikšmės.
+$length = 5;
+$height = 4;
+*  *  *  *  *
+*  *  *  *  *
+*  *  *  *  *
+*  *  *  *  *
+*/
+
+/*
+14. Išspausdinkite skaičių nuo 1 iki x daliklius, mažesnius už patį skaičių.
+1:
+2: 1
+3: 1
+4: 1 2
+5: 1
+6: 1 2 3
+7: 1
+8: 1 2 4
+9: 1 3
+10: 1 2 5
+11: 1
+12: 1 2 3 4 6
+...
+ */
---
+++ PHPIntro/lesson4/exercises.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+// Visur sudėkite reikiamus parametrų bei return tipus
+
+/*
+1. Parašykite funkciją 'dividesBy5', kuri priimtų int tipo skaičių ir grąžintų jo dalybos iš 5 liekaną.
+*/
+
+/*
+2. Parašykite funkciją 'arrayPrinter', kuri priimtų array tipo parametrą ir
+išspausdintų kiekvieną masyvo elementą naujoje eilutėje.
+Funkcijos kvietimas: arrayPrinter(['some text', 'another text'])
+Funkcija grąžina: funkcija nieko negrąžina - ji tik išspausdina masyvą į terminalą:
+'some text'
+'another text'
+...
+*/
+
+/*
+3. Parašykite funkciją 'stringEnhancer', kuri grąžintų pakeistą tekstą.
+Funkcijos kvietimas: stringEnhancer('some text', '##')
+Funkcija grąžina: '##some text##'
+Funkcijos kvietimas: stringEnhancer('some text')
+Funkcija grąžina: '**some text**'
+*/
+
+/*
+4. Parašykite funkciją 'stringModifier', kuri pamodifikuotų paduotą string tipo kintamąjį.
+Funkcijos kvietimas:
+$x = 'some text';
+stringModifier($x, '##');
+echo $x; // '##some text##'
+Funkcija grąžina: funkcija nieko negrąžina
+*/
+
+/*
+5. Parašykite funkciją 'textReplicator', kuri grąžintų 'padaugintą' tekstą.
+Funkcijos kvietimas:
+textReplicator('some_text', 3);
+Funkcija grąžina: 'some_text-some_text-some_text'
+Funkcijos kvietimas:
+textReplicator('some_text', null);
+Funkcija grąžina: 'some_text'
+*/
+
+/*
+6. Paverskite funkciją 'textReplicator', į veikiančią anoniminę funkciją.
+*/
+
+/*
+7. Parašykite funkciją, kuri spausdintų šią struktūrą. Funkcijai turi būti paduodama skaičius, kuris nurodys
+kiek lygių turi būti spausdinama. Pavyzdžio atveju, tas skaičius yra 5.
+    1
+   222
+  33333
+ 4444444
+555555555
+*/
+
+/*
+8. Parašykite funkciją, kuri leistų vartotojui spėti, kokį random skaičių sugeneravo scriptas. Atspėjus, outputas turi
+informuoti, kiek spėjimų buvo padaryta.
+Input'ui iš vartotojo gauti naudokite funkciją 'readline' - https://www.php.net/manual/en/function.readline.php
+Atsitiktiniam skaičiui generuoti naudokite funkciją 'rand' - https://www.php.net/manual/en/function.rand.php
+Kvietimo pavyzdys:
+php -f exercises.php
+Įveskite galimų skaičių intervalo galq: 10
+Spėkite skaičių: 5
+Aukščiau!
+Spėkite skaičių: 9
+Žemiau!
+Spėkite skaičių: 8
+Teisingai! Panaudota spėjimų - 2
+*/
