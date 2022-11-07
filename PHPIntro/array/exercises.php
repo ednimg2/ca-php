307fd4660df752ef931e08258e3d20798f5e6cb6
Mindaugas <mindaugas.g@rrr.lt>
2022-11-07 20:49:59 +0200

    Create exercises array

--- PHPIntro/array/exercises.php
+++ PHPIntro/array/exercises.php
@@ -58,3 +58,60 @@ Masyvą gausite iškvietę funkciją 'getNumbers'
 15. Raskite ir grąžinkite sandaugą tų masyvo narių, kurie dalijasi iš 5
 16. Į masyvą pridėkite naujų narių - skaičius nuo 255 iki 299 - ir išspausdinkite masyvą pasinaudodami funkcija 'printr'
 */
+
+function exercise4(int $id)
+{
+    $prefix = 'product_';
+
+    $products = [
+        'product_1' => 'desk',
+        'product_2' => 'lamp',
+        'product_3' => 'sofa',
+    ];
+
+    /*
+    Sunaikinkitę visas $products masyve esančias reikšmes, kurių raktai yra prefikso ir ID junginys t.y. ($prefix . $id)
+    Grąžinkite pamodifikuotą masyvą.
+    Jeigu tokio rakto ($id) nėra, gražinkite null.
+    Funkcijai pridėkite trūkstamą return tipą.
+    Funkcijos kvietimas: exercise2(1)
+    */
+
+    return [];
+}
+
+function exercise5(): array
+{
+    $transactions = [
+        [
+            'total' => 200,
+            'status' => 'error',
+        ],
+        [
+            'total' => 150,
+            'status' => 'completed',
+        ],
+    ];
+
+    /*
+    Sunaikinkite visus elementus, kurių 'status' reikšmė yra 'error' ir grąžinkite pamodifikuotą masyvą.
+    Tikėkitės, kad $transactions masyvo narių skaičius gali varijuoti, tad naudokite ciklą.
+    */
+
+    return [];
+}
+
+function exercise6(string $key)
+{
+    $products = [
+        'product_1' => 'shirt',
+        'product_2' => 'trousers',
+        'product_98' => 'coat',
+    ];
+    /*
+    Funkcija turi grąžinti reikšmę pagal paduotą raktą.
+    Jeigu paduotas raktas neegzistuoja $products masyve, grąžinkite null.
+    Pridėkite teisingą return tipą.
+    Funkcijos kvietimas: exercise4('product_2')
+    */
+}
