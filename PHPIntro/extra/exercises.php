aa807148ce973c8e73d45fff6783fb6e4717880d
Mindaugas <mindaugas.g@rrr.lt>
2022-11-21 16:51:13 +0200

    create extra exercises

---
+++ PHPIntro/extra/exercises.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * Keliamieji metai. Parasykite funkcija, kuriai padavus metus (pvz.: 2020)
+ * grazintu true - keliamieji metai, false - ne keliamieji
+ * Pridekite teisinga return tipa
+ *
+ * Truputis info:
+ *
+ * The year can be evenly divided by 4,
+ * If the year can be evenly divided by 100, it is NOT a leap year, unless;
+ * The year is also evenly divisible by 400. Then it is a leap year.
+ */
+
+function isLeapYear(int $year)
+{
+
+}
+
+/*
+ * Parasykite funkcija, kuri apskaiciuoti kiek palukanu gausite is banko po x metu, jei palukanos y.
+ * Pridekite teisinga return tipa
+ */
+
+function calculateInterestSum(float $deposit, float $interest)
+{
+
+}
+
+/*
+ * Parasykite funkcija, kuri isspausdintu fibonacci seka nuo 0 iki nuorodyto skaiciaus
+ * Pridekite teisinga return tipa
+ * 0, 1, 1, 2, 3, 5, 8, 13
+ */
+
+function printFibonacci()
+{
+
+}
+
+/*
+ * Parašykite funkciją, kuri apskaičiuotu, paduoto sveiko skaičiaus faktorialą.
+ * Pridėkite teisingą return tipą.
+ *
+ * Kaip apskaičiuojamas faktorialas:
+ * n! = n * (n-1) * (n-2) * ... * 3 * 2 * 1
+ * arba
+ * n! = 1 * 2 * 3 * ... * n
+ *
+ * 2! = 1 * 2 = 2
+ * 3! = 1 * 2 * 3 = 6
+ *
+ */
+
+function printFactorial()
+{
+
+}
